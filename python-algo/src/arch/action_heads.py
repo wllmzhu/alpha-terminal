fix action masking disallow action_type when all locations are unavailable otherwise Categorical.sample will pick unavailable location

python-algo/src/arch/action_heads.py
@@ -13,10 +13,9 @@ class ActionTypeHead(nn.Module):
         self.fc1 = nn.Linear(256, 64)
         self.fc2 = nn.Linear(64, 9)
 
-    def forward(self, x, game_state):
+    def forward(self, x, mask):
         x = F.relu(self.fc1(x))
         logits = self.fc2(x)
-        mask = self.valid_action_type_mask(game_state)
         logits = torch.where(mask.to(self.device), logits, torch.tensor(-1e+8).to(self.device))
         
         dist = Categorical(logits=logits)
@@ -24,70 +23,18 @@ class ActionTypeHead(nn.Module):
         logp = dist.log_prob(action_type)
         return action_type.item(), logits, logp
 
-    def valid_action_type_mask(self, game_state):
-        # NOOP is always available
-        # structure and mobile units must be affordable
-        # upgrade and remove are valid if there exists structure
-        
-        # NOTE: gamestate=None for SL
-        if game_state is None:
-            return torch.tensor([True] * 9)
-
-        mask = [True] * 9
-        # check affordability
-        for unit in constants.STRUCTURES + constants.MOBILES:
-            mask[unit] = mask[unit] and game_state.number_affordable(constants.ACTION_SHORTHAND[unit]) > 0
-        # if there is any structure not pending removal
-        mask[constants.REMOVE]  = any(map(
-            lambda loc: game_state.contains_stationary_unit(loc) and not game_state.game_map[loc][0].pending_removal, 
-            constants.MY_LOCATIONS))
-        # if there is any structure
-        mask[constants.UPGRADE] = any(map(game_state.contains_stationary_unit, constants.MY_LOCATIONS))
-        mask = torch.tensor(mask)
-        return mask
-
-
 class LocationHead(nn.Module):
     def __init__(self, device):
         super().__init__()
         self.device = device
         self.fc1 = nn.Linear(256, 210)
-        self.flush_queue_mask()
 
-    def flush_queue_mask(self):
-        self.already_removing = set() 
-        self.already_upgrading = set()
-
-    def forward(self, x, game_state, action_type):
+    def forward(self, x, mask):
         logits = self.fc1(x)
-        mask = self.valid_location_mask(game_state, action_type)
         logits = torch.where(mask.to(self.device), logits, torch.tensor(-1e+8).to(self.device))
         
         dist = Categorical(logits=logits)
         location = dist.sample()
         logp = dist.log_prob(location)
-        location_xy = constants.MY_LOCATIONS[location.item()]
 
-        if action_type == constants.REMOVE:
-            self.already_removing.add(tuple(location_xy))
-        elif action_type == constants.UPGRADE:
-            self.already_upgrading.add(tuple(location_xy))
-        
-        return location_xy, logits, logp
-
-    def valid_location_mask(self, game_state, action_type):
-        # NOTE: gamestate=None for SL
-        if game_state is None or action_type == constants.NOOP:             # SL or NOOP
-            mask = [True] * 210
-        elif action_type in constants.STRUCTURES + constants.MOBILES:       # structure or mobile
-            mask = [game_state.can_spawn(constants.ACTION_SHORTHAND[action_type], loc) for loc in constants.MY_LOCATIONS]
-        elif action_type == constants.REMOVE:                               # remove
-            mask = [bool(game_state.contains_stationary_unit(loc)) 
-                    and tuple(loc) not in self.already_removing
-                    for loc in constants.MY_LOCATIONS]
-        else:                                                               # upgrade
-            mask = [bool(game_state.contains_stationary_unit(loc)) 
-                    and tuple(loc) not in self.already_upgrading
-                    for loc in constants.MY_LOCATIONS]
-        mask = torch.tensor(mask)
-        return mask
+        return constants.MY_LOCATIONS[location.item()], logits, logp

python-algo/src/arch/model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 from .encoders import SpatialEncoder, ScalarEncoder
 from .action_heads import ActionTypeHead, LocationHead
+from .. import constants
 
 class FeatureEncoder(nn.Module):
     def __init__(self):
@@ -23,11 +24,20 @@ class PolicyNet(nn.Module):
         self.lstm = nn.LSTMCell(input_size=256, hidden_size=256)
         self.action_type_head = ActionTypeHead(device=device)
         self.location_head = LocationHead(device=device)
+        self.flush_queue_mask()
     
     def forward(self, observation_feature, game_state, hidden_and_cell_states):
         hidden_state, cell_state = self.lstm(observation_feature, hidden_and_cell_states)
-        action_type, action_type_logits, action_type_logp = self.action_type_head(hidden_state, game_state)
-        location, location_logits, location_logp = self.location_head(hidden_state, game_state, action_type)
+        
+        action_type_mask = get_action_type_mask(game_state, self.already_removing)
+        action_type, action_type_logits, action_type_logp = self.action_type_head(hidden_state, action_type_mask)
+        
+        location_mask = get_location_mask(game_state, action_type, self.already_removing)
+        location, location_logits, location_logp = self.location_head(hidden_state, location_mask)
+        
+        if action_type == constants.REMOVE:
+            self.already_removing.add(tuple(location))
+        
         return action_type, action_type_logits, action_type_logp, \
             location, location_logits, location_logp, \
             (hidden_state, cell_state)
@@ -36,4 +46,43 @@ class PolicyNet(nn.Module):
         return (torch.zeros(batch_size, 256).to(self.device), torch.zeros(batch_size, 256).to(self.device))
 
     def flush_queue_mask(self):
-        self.location_head.flush_queue_mask()
+        self.already_removing = set()
+
+def get_action_type_mask(game_state, already_removing):
+    # NOOP is always available
+    # structure and mobile units must be affordable and spawnable
+    # upgrade is valid if there exists structure
+    # remove is valid if there exists structure which is not pending removal 
+    #   (otherwise agent learns to exploit this invalid action)
+    
+    # NOTE: gamestate=None for SL
+    if game_state is None:
+        return torch.tensor([True] * 9)
+
+    mask = [True] * 9
+    for unit in constants.STRUCTURES + constants.MOBILES:
+        # affordable
+        mask[unit] = mask[unit] and game_state.number_affordable(constants.ACTION_SHORTHAND[unit]) > 0
+        # spawnable
+        mask[unit] = mask[unit] and any(map(lambda loc: game_state.can_spawn(constants.ACTION_SHORTHAND[unit], loc), constants.MY_LOCATIONS))
+    # if there is any structure not pending removal
+    mask[constants.REMOVE]  = any(map(lambda loc: game_state.contains_stationary_unit(loc) and tuple(loc) not in already_removing, constants.MY_LOCATIONS))
+    # if there is any structure
+    mask[constants.UPGRADE] = any(map(game_state.contains_stationary_unit, constants.MY_LOCATIONS))
+    mask = torch.tensor(mask)
+    return mask
+
+def get_location_mask(game_state, action_type, already_removing):
+    # NOTE: gamestate=None for SL
+    if game_state is None or action_type == constants.NOOP:             # SL or NOOP
+        mask = [True] * 210
+    elif action_type in constants.STRUCTURES + constants.MOBILES:       # structure or mobile
+        mask = [game_state.can_spawn(constants.ACTION_SHORTHAND[action_type], loc) for loc in constants.MY_LOCATIONS]
+    elif action_type == constants.REMOVE:                               # remove
+        mask = [bool(game_state.contains_stationary_unit(loc)) 
+                and tuple(loc) not in already_removing
+                for loc in constants.MY_LOCATIONS]
+    else:                                                               # upgrade
+        mask = [bool(game_state.contains_stationary_unit(loc)) for loc in constants.MY_LOCATIONS]
+    mask = torch.tensor(mask)
+    return mask